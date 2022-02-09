cd38deec084fd08032660b7887e559708a3863ef
TzviaDavidzada <tzvia.ye@gmail.com>
2022-02-10 00:09:08 +0200

    push python

---
+++ zip_job.py
@@ -0,0 +1,20 @@
+import os
+from zipfile import ZipFile
+
+array = ['a','b','c','d']
+
+for letter in array:
+    fileName = letter + '.txt'
+    open(fileName, "x")
+    
+    if not os.path.exists(fileName):
+        raise Exception('text file ' + fileName + 'was not created')
+    
+    # TODO os.environ.get('VERSION')
+    VERSION = "1.2"
+    zipName = letter + '_' + VERSION + '.zip'
+    with ZipFile(zipName, 'w') as zipObj:
+        zipObj.write(fileName)
+    
+    if not os.path.exists(zipName):
+        raise Exception('zip file ' + zipName + 'was not created')
